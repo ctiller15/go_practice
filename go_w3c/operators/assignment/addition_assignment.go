c0ce03d66c9b167ecbe77a6f4306868f6b87e116
Christopher Tiller <christophertiller@ChristophersMBP.lan>
2024-04-17 23:33:16 -0400

    complete both arrays and operators sections

---
+++ go_w3c/operators/assignment/addition_assignment.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 10
+	x += 5
+	fmt.Println(x)
+}
---
+++ go_w3c/operators/comparison/greater_than.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 5
+	var y = 3
+	fmt.Println(x > y) // returns 1
+}
